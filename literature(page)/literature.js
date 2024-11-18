8345441931ecce4149108ddf73b93c53b1029eb0
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2024-11-18 14:55:22 +0200

    first commit

---
+++ literature(page)/literature.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionBtn = document.querySelector(".accordion-btn");
+    const accordionContent = document.querySelector(".accordion-content");
+
+    accordionBtn.addEventListener("click", function () {
+        if (accordionContent.style.maxHeight) {
+            // Закриваємо акордеон
+            accordionContent.style.maxHeight = null;
+        } else {
+            // Відкриваємо акордеон
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        }
+    });
+});
